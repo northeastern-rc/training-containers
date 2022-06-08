6200a5b0c9f884f5afb0822220c752ba1db6b38b
marianalevineu <m.levi@northeastern.edu>
2022-06-08 08:49:29 -0400

    modified rest of scripts with tmp dir info and path to scripts info

--- Exercise_3/submit.bash
+++ Exercise_3/submit.bash
@@ -6,14 +6,25 @@
 #SBATCH -J test.tf-container
 #SBATCH -p gpu
 #SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --output=ex3.out
+#SBATCH --error=ex3.err
+
+## Note - this script assumes the python script is in the present working directory
+## If it's not there, copy it first by modifying and uncommenting the follwing line:
+# cp /path/to/train_my_model-pytorch.py .
 
 ## Load the singularity module
 module load singularity/3.5.3
+
+# /tmp folder will be replaced by the folder path you provide. We recommend this as /tmp is limited to 20GB (and shared with other users on the given compute nodes).
+export SINGULARITY_TMPDIR=/scratch/$USER/$SLURM_JOB_ID
+mkdir -p $SINGULARITY_TMPDIR
+
 ## set the path to the gromacs container
 CONTAINER=/shared/container_repository/pytorch/pytorch_22.05-py3.sif 
-## Enter your desired workspace, and place the script inside. The following is commented out as it assumes the script is in the present working directory.
-# cd /path/to/my/workdir
-# cp /path/to/tpr .
 MY_SCRIPT=train_my_model-pytorch.py
 ## Run TF with your script, append any additional paths you'll need using the -B options. In this example we append the /scratch/$USER folder (not actually needed for this example):
 singularity run --nv -B /scratch/$USER:/mnt $CONTAINER python3 $MY_SCRIPT
+
+# remove the tmp folder to avoid clutter:
+rm -rf $SINGULARITY_TMPDIR
--- Exercise_4/submit.bash
+++ Exercise_4/submit.bash
@@ -7,13 +7,23 @@
 #SBATCH -p gpu
 #SBATCH --gres=gpu:v100-sxm2:1
 
+## Note - this script assumes that benchmark.tpr is in the present working directory
+## If it's not there, copy it first by modifying and uncommenting the follwing line:
+# cp /path/to/benchmark.tpr .
+
 ## Load the singularity module
 module load singularity/3.5.3
+
+# /tmp folder will be replaced by the folder path you provide. We recommend this as /tmp is limited to 20GB (and shared with other users on the given compute nodes).
+export SINGULARITY_TMPDIR=/scratch/$USER/$SLURM_JOB_ID
+mkdir -p $SINGULARITY_TMPDIR
+
 ## set the path to the gromacs container
 CONTAINER=/shared/container_repository/gromacs/gromacs-2020_2.sif
-## Enter your desired workspace, and place the tpr file inside. The following is commented out as it assumes the TPR file is in the present working directory.
-# cd /path/to/my/workdir
-# cp /path/to/tpr .
 MYTPR=benchmark.tpr
+
 ## Run GROMACAS - using a GPU and 8 threads (CPUs) :
 singularity run --nv $CONTAINER gmx mdrun -ntomp 8 -s $MYTPR -v -noconfout -dlb yes
+
+# remove the tmp folder to avoid clutter:
+rm -rf $SINGULARITY_TMPDIR
